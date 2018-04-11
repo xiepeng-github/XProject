8408219e7641ecd97a4b92a5e4df94f5ac3bfb95
xiepeng <xpsunshine@163.com>
2018-04-11 10:50:13 +0800

    xiepeng_surface_解决每次重启idea后 language level被重置为1.5的问题

--- mavenXJavaSE/src/main/java/com/xproject/util/XHelloWorld.java
+++ mavenXJavaSE/src/main/java/com/xproject/util/XHelloWorld.java
@@ -1,7 +1,21 @@
 package com.xproject.util;
 
 public class XHelloWorld {
-    public static void main(String[] agrs) {
-        System.out.println("XHello World");
+
+    public static void main(String[] args){
+        Object o=new Object(){
+            public boolean equals(Object obj){
+                return true;
+            }
+        };
+        System.out.println(o.equals("Fred"));
+        System.out.println(Math.round(11.4));
+        System.out.println(Math.round(-11.4));
+        System.out.println(Math.floor(0.5));
     }
+
+    static void f_a() {
+
+    }
+
 }
